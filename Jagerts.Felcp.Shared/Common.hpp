787721bf94f8b9f9558dee993f39a31c70e98903
Jagreaper <jamesalexgrant@hotmail.com>
2018-01-02 09:34:08 +1000

    Moved Helpers and Imaging dll's over

--- Jagerts.Felcp.Shared/Common.hpp
+++ Jagerts.Felcp.Shared/Common.hpp
@@ -17,3 +17,15 @@
 #define JAGERTS_FELCP_IO_XML_API __declspec(dllimport)
 #endif
 #endif
+
+#ifdef JAGERTS_FELCP_HELPERS_DLL
+#define JAGERTS_FELCP_HELPERS_API __declspec(dllexport)
+#else
+#define JAGERTS_FELCP_HELPERS_API __declspec(dllimport)
+#endif
+
+#ifdef JAGERTS_FELCP_IMAGING_DLL
+#define JAGERTS_FELCP_IMAGING_API __declspec(dllexport)
+#else
+#define JAGERTS_FELCP_IMAGING_API __declspec(dllimport)
+#endif
