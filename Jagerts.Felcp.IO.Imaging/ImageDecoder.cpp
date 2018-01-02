27bf6c79f256e87c81e90281f31fd0a06d57f54e
Jagreaper <jamesalexgrant@hotmail.com>
2018-01-02 10:19:18 +1000

    Added RawImageData type to image decoder

--- Jagerts.Felcp.IO.Imaging/ImageDecoder.cpp
+++ Jagerts.Felcp.IO.Imaging/ImageDecoder.cpp
@@ -16,48 +16,115 @@ void FreeImage(unsigned char* data)
 	stbi_image_free(data);
 }
 
-bool ImagePathDecoder::TryDecode(const char* source, Image* output, void* arg)
+void RawImageData::Free(RawImageData* data)
 {
-	int width, height, comp;
-	unsigned char* data = LoadImageFromFile(source, &width, &height, &comp, STBI_rgb_alpha);
-	int data_length = width * height * comp;
+	stbi_image_free(data->_data);
+	data->_size = 0;
+}
 
-	output->SetSize(width, height);
-	output->SetData(data, data_length);
+void RawImageData::SetData(unsigned char* data, int size)
+{
+	if (this->_data != NULL)
+		RawImageData::Free(this);
 
-	if (comp == 1)
+	this->_size = size;
+	this->_data = data;
+}
+
+void RawImageData::SetWidth(int width)
+{
+	this->_width = width;
+}
+
+void RawImageData::SetHeight(int height)
+{
+	this->_height = height;
+}
+
+void RawImageData::SetPixelSize(int pixel_size)
+{
+	this->_pixel_size = pixel_size;
+}
+
+const unsigned char* RawImageData::GetData() const
+{
+	return this->_data;
+}
+
+const int RawImageData::GetWidth() const
+{
+	return this->_width;
+}
+
+const int RawImageData::GetHeight() const
+{
+	return this->_height;
+}
+
+const int RawImageData::GetPixelSize() const
+{
+	return this->_pixel_size;
+}
+
+const size_t RawImageData::GetSize() const
+{
+	return this->_size;
+}
+
+void RawImageData::ToImage(Image* image)
+{
+	image->SetSize(this->_width, this->_height);
+	image->SetData(this->_data, this->_size);
+	if (this->_pixel_size == 1)
 	{
 		ChannelInfo info[1];
 		info[0].Type = ChannelType::Gray;
-		output->SetChannels(info, comp);
+		image->SetChannels(info, this->_pixel_size);
 	}
-	else if (comp == 2)
+	else if (this->_pixel_size == 2)
 	{
 		ChannelInfo info[2];
 		info[0].Type = ChannelType::Gray;
 		info[1].Type = ChannelType::Alpha;
-		output->SetChannels(info, comp);
+		image->SetChannels(info, this->_pixel_size);
 	}
-	else if (comp == 3)
+	else if (this->_pixel_size == 3)
 	{
 		ChannelInfo info[3];
 		info[0].Type = ChannelType::Red;
 		info[1].Type = ChannelType::Green;
 		info[2].Type = ChannelType::Blue;
-		output->SetChannels(info, comp);
+		image->SetChannels(info, this->_pixel_size);
 	}
-	else if (comp == 4)
+	else if (this->_pixel_size == 4)
 	{
 		ChannelInfo info[4];
 		info[0].Type = ChannelType::Red;
 		info[1].Type = ChannelType::Green;
 		info[2].Type = ChannelType::Blue;
 		info[3].Type = ChannelType::Alpha;
-		output->SetChannels(info, comp);
+		image->SetChannels(info, this->_pixel_size);
 	}
 
-	output->CreateDefaults();
-	FreeImage(data);
+	image->CreateDefaults();
+}
 
+bool ImagePathDecoder::TryDecode(const char* source, RawImageData* output, void* arg)
+{
+	int width, height, comp;
+	unsigned char* data = LoadImageFromFile(source, &width, &height, &comp, STBI_rgb_alpha);
+	output->SetWidth(width);
+	output->SetHeight(height);
+	output->SetPixelSize(comp);
+	output->SetData(data, width * height * comp);
+	return true;
+}
+
+bool ImagePathDecoder::TryDecode(const char* source, Image* output, void* arg)
+{
+	RawImageData data;
+	this->TryDecode(source, &data, arg);
+	data.ToImage(output);
+	RawImageData::Free(&data);
 	return true;
 }
--- Jagerts.Felcp.IO.Imaging/ImageDecoder.hpp
+++ Jagerts.Felcp.IO.Imaging/ImageDecoder.hpp
@@ -10,9 +10,35 @@ namespace Jagerts::Felcp::Imaging
 
 namespace Jagerts::Felcp::IO
 {
-	class JAGERTS_FELCP_IO_IMAGING_API ImagePathDecoder : public DecoderBase<const char*, Jagerts::Felcp::Imaging::Image*, void*>
+	class RawImageData
+	{
+	public:
+		static void Free(RawImageData* data);
+
+		void SetData(unsigned char* data, int size);
+		void SetWidth(int width);
+		void SetHeight(int height);
+		void SetPixelSize(int pixel_size);
+
+		const unsigned char* GetData() const;
+		const int GetWidth() const;
+		const int GetHeight() const;
+		const int GetPixelSize() const;
+		const size_t GetSize() const;
+		
+		void ToImage(Jagerts::Felcp::Imaging::Image* image);
+	private:
+		unsigned char* _data;
+	    int _width;
+		int _height;
+		int _pixel_size;
+		size_t _size;
+	};
+
+	class JAGERTS_FELCP_IO_IMAGING_API ImagePathDecoder : public PathDecoderBase<Jagerts::Felcp::Imaging::Image*, void*>, public PathDecoderBase<RawImageData, void*>
 	{
 	public:
 		bool TryDecode(const char* source, Jagerts::Felcp::Imaging::Image* output, void* arg);
+		bool TryDecode(const char* source, RawImageData* output, void* arg);
 	};
 }
