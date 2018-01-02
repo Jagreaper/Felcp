0638337f801330b3bac08791e98266480346ab65
Jagreaper <jamesalexgrant@hotmail.com>
2018-01-02 12:48:18 +1000

    Removed headers

--- dist/Jagerts.Felcp.Helpers/PrimitiveTypes.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Jagerts::Felcp::Helpers
-{
-	using byte = unsigned char;
-	using ushort = unsigned short;
-	using uint = unsigned int;
-	using ulong = unsigned long long;
-}
